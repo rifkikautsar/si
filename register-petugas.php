e6478713ef3c602cffcb3e0fc8fcace1f480a3f0
AvYour <83781875+AvYour@users.noreply.github.com>
2021-07-30 08:25:13 +0700

    Update register-petugas

--- register-petugas.php
+++ register-petugas.php
@@ -1,30 +1,38 @@
 <head>
+    <style>
+    h1 {
+        font-size: 20px;
+    }
+    </style>
+    <div class="container" style="color: black;">
+        <div class="mx-4">
+            <h1>Register Petugas</h1>
+        </div>
 
 <body>
-    <div class="container" style="color: black;">
-        <div class="mx-4 mt-4 mb-2">
-            <label for="id_petugas class=" form-label">ID Petugas</label>
-            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
-        </div>
-        <div class="mx-4 mb-2">
-            <label for="nama_petugas class=" form-label">Nama Petugas</label>
-            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
-        </div>
-        <div class="mx-4 mb-2">
-            <label for="hak_akses class=" form-label">Hak Akses</label>
-            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
-        </div>
-        <div class="mx-4 mb-2">
-            <label for="username=" form-label">Username</label>
-            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
-        </div>
-        <div class="mx-4 mb-2">
-            <label for="password class=" form-label">Password</label>
-            <input class="form-control form-control-sm" style="width: 1000px;" type="password">
-        </div>
-        <div class="mx-4 mb-2">
-            <label for="re_password class=" form-label">Re-Enter Password</label>
-            <input class="form-control form-control-sm" style="width: 1000px;" type="password">
-        </div>
+    <div class="mx-4 mt-4 mb-2">
+        <label for="id_petugas class=" form-label">ID Petugas</label>
+        <input class="form-control form-control-sm" name="id_petugas" style="width: 1000px;" type="text">
+    </div>
+    <div class="mx-4 mb-2">
+        <label for="nama_petugas class=" form-label">Nama Petugas</label>
+        <input class="form-control form-control-sm" name="nama_petugas" style="width: 1000px;" type="text">
+    </div>
+    <div class="mx-4 mb-2">
+        <label for="hak_akses class=" form-label">Hak Akses</label>
+        <input class="form-control form-control-sm" name="hak_akses" style="width: 1000px;" type="text">
+    </div>
+    <div class="mx-4 mb-2">
+        <label for="username=" form-label">Username</label>
+        <input class="form-control form-control-sm" name="username" style="width: 1000px;" type="text">
+    </div>
+    <div class="mx-4 mb-2">
+        <label for="password class=" form-label">Password</label>
+        <input class="form-control form-control-sm" name="password" style="width: 1000px;" type="password">
+    </div>
+    <div class="mx-4 mb-2">
+        <label for="re_password class=" form-label">Re-Enter Password</label>
+        <input class="form-control form-control-sm" name="re_password" style="width: 1000px;" type="password">
+    </div>
     </div>
 </body>
