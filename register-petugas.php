8233c88451ad6b851691cf7d06209a79c7122003
AvYour <83781875+AvYour@users.noreply.github.com>
2021-07-30 08:05:23 +0700

    page register petugas

---
+++ register-petugas.php
@@ -0,0 +1,30 @@
+<head>
+
+<body>
+    <div class="container" style="color: black;">
+        <div class="mx-4 mt-4 mb-2">
+            <label for="id_petugas class=" form-label">ID Petugas</label>
+            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
+        </div>
+        <div class="mx-4 mb-2">
+            <label for="nama_petugas class=" form-label">Nama Petugas</label>
+            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
+        </div>
+        <div class="mx-4 mb-2">
+            <label for="hak_akses class=" form-label">Hak Akses</label>
+            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
+        </div>
+        <div class="mx-4 mb-2">
+            <label for="username=" form-label">Username</label>
+            <input class="form-control form-control-sm" style="width: 1000px;" type="text">
+        </div>
+        <div class="mx-4 mb-2">
+            <label for="password class=" form-label">Password</label>
+            <input class="form-control form-control-sm" style="width: 1000px;" type="password">
+        </div>
+        <div class="mx-4 mb-2">
+            <label for="re_password class=" form-label">Re-Enter Password</label>
+            <input class="form-control form-control-sm" style="width: 1000px;" type="password">
+        </div>
+    </div>
+</body>
