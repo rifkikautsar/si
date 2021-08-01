027b5a5c30eea452c518d2752d55f4ab5df3d4bc
biner009 <rifkikautsar1@gmail.com>
2021-08-01 11:50:05 +0700

    ganti font poppins

--- pages/admin/sidebar.php
+++ pages/admin/sidebar.php
@@ -10,9 +10,12 @@
 
     <!-- Custom fonts for this template-->
     <link href="../../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
-    <link
+    <!-- <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet" />
+        rel="stylesheet" /> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href="../../assets/css/sb-admin-2.min.css" rel="stylesheet" />
@@ -25,6 +28,7 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!--==================== UNICONS ====================-->
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+    <link rel="stylesheet" href="../../assets/css/styles.css">
 </head>
 
 <body id="page-top">
