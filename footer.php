e8303267c8f9e2652e34e20dc4f7337857ab3aac
biner009 <rifkikautsar1@gmail.com>
2021-08-02 11:58:21 +0700

    fix footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
  <footer class="sticky-footer bg-white">
      <div class="container my-auto">
          <div class="copyright text-center my-auto">
-             <span>&copy;iCendol. All Right Reserved</span>
+             <span>&copy;iLab. All Right Reserved</span>
          </div>
      </div>
  </footer>
